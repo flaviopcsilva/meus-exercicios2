723b5021b224e3218898babaf77e3531bf85992b
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-21 20:54:25 -0300

    Exercício 01 de objetos

--- aula-objetos/exercicio01.js
+++ aula-objetos/exercicio01.js
@@ -1,1 +1,16 @@
+const pessoa = {
+  nome : "Flávio",
+  idade : 18,
+  altura : 1.67,
+  temCNH : true,
+  apelidos : ["Flaviano", "Battousai"]
+};
 
+let possuiCNH = "";
+
+if (pessoa.temCNH){
+  console.log("possui CNH");
+} else console.log("não possui CNH");
+
+console.log(`${pessoa.nome} tem ${pessoa.idade} anos, ${pessoa.altura}m, ${possuiCNH} e os apelidos:
+${pessoa.apelidos}`);
