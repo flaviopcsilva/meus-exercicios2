65210966586977990ae1fe8d9b6dbfcf8c0ffd24
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-22 05:11:26 -0300

    Começando exércicio 02

--- aula-objetos/exercicio02.js
+++ aula-objetos/exercicio02.js
@@ -1,1 +1,17 @@
+const produtos = [
+  {
+    nome : "Pão",
+    precounit : 100,
+    quantidade : 3
+  }, {
+    nome : "Queijo",
+    preco : 500,
+    quantidade : 5,
+  }, {
+    nome : "Leite",
+    preco : 900,
+    quantidade : 3
+  }
+  
+  ];
 
