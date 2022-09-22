7c557d3f2b7ad9ede165caa774542d63ec2b45f9
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-21 21:21:05 -0300

    Usando ternário para CNH

--- aula-objetos/exercicio01.js
+++ aula-objetos/exercicio01.js
@@ -6,11 +6,11 @@ const pessoa = {
   apelidos : ["Flaviano", "Battousai", "Flavinho"]
 };
 
-let possuiCNH = "";
-
-if (pessoa.temCNH){
+let possuiCNH = pessoa.temCNH?"possui CNH":"não possui CNH";
+/*if (pessoa.temCNH){
   console.log("possui CNH");
 } else console.log("não possui CNH");
+*/
 
 console.log(`${pessoa.nome} tem ${pessoa.idade} anos, ${pessoa.altura}m, ${possuiCNH} e os apelidos:`);
 for (let apelido of pessoa.apelidos){
