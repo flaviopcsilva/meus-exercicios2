29d5698822f15adac6eabd6083e23d52205778a3
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-22 05:29:24 -0300

    Dando algum erro de shorthand

--- aula-objetos/exercicio02.js
+++ aula-objetos/exercicio02.js
@@ -1,17 +1,46 @@
-const produtos = [
+/*const produtos = [
   {
     nome : "Pão",
     precounit : 100,
     quantidade : 3
   }, {
     nome : "Queijo",
-    preco : 500,
+    precounit : 500,
     quantidade : 5,
   }, {
     nome : "Leite",
-    preco : 900,
+    precounit : 900,
     quantidade : 3
   }
   
-  ];
+  ]; */
 
+const cartao = {
+  nome = "Flavio",
+  idade : 41,
+  produtos :[
+  {
+    nome : "Pão",
+    precounit : 100,
+    quantidade : 3
+  }, {
+    nome : "Queijo",
+    precounit : 500,
+    quantidade : 5,
+  }, {
+    nome : "Leite",
+    precounit : 900,
+    quantidade : 3
+  }
+  
+  ]
+};
+
+let total = 0;
+
+/*for (let i = 0; i<produtos.length; i++){
+  total += produtos[i].precounit * produtos[i].quantidade;
+}
+console.log (`Cliente : ${cartao.nome}
+total a pagar : ${total}`); */
+console.log(cartao);
