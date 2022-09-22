5021b522d75865c36b105f2842689ebb19ca0509
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-22 06:18:07 -0300

    Exercicio resolvido

--- aula-objetos/exercicio02.js
+++ aula-objetos/exercicio02.js
@@ -1,19 +1,24 @@
 const produtos = [
   {
     nome : "Pão",
-    precounit : 1000,
+    precounit : 100,
     quantidade : 3
   }, {
     nome : "Queijo",
-    precounit : 5000,
+    precounit : 500,
     quantidade : 5
   }, {
     nome : "Leite",
-    precounit : 9000,
+    precounit : 900,
     quantidade : 3
   }
-  
-  ]; 
+]; 
+
+const cartao= {
+  nome : "Flavio",
+  idade : 41,
+  produtos
+}
 
 /*const cartao = {
   nome = "Flavio",
@@ -37,10 +42,16 @@ const produtos = [
 
 let total = 0;
 
-for (let i = 0; i<produtos.length; i++){
+/*for (let i = 0; i<produtos.length; i++){
   total += produtos[i].precounit * produtos[i].quantidade;
+}*/ 
+// pode ser assim tambem:
+
+for (let produto of produtos){
+  total += produto.precounit * produto.quantidade;
 }
+
 /*console.log (`Cliente : ${cartao.nome}
 total a pagar : ${total}`); */
-console.log(produtos);
-console.log(total/1000);
+const totalFormatado = (total/100).toFixed(2);
+console.log(`Sr(a) ${cartao.nome}, o total a pagar é R$ ${totalFormatado}.`);
