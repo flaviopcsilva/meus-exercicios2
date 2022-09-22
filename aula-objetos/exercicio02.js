0e4bcc1e10871c01a0cfe5c9b8f6eedf28fe9c78
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-22 05:38:21 -0300

    Dando algum erro de shorthand de novo 01

--- aula-objetos/exercicio02.js
+++ aula-objetos/exercicio02.js
@@ -1,46 +1,46 @@
-/*const produtos = [
+const produtos = [
   {
     nome : "Pão",
-    precounit : 100,
+    precounit : 1000,
     quantidade : 3
   }, {
     nome : "Queijo",
-    precounit : 500,
-    quantidade : 5,
-  }, {
-    nome : "Leite",
-    precounit : 900,
-    quantidade : 3
-  }
-  
-  ]; */
-
-const cartao = {
-  nome = "Flavio",
-  idade : 41,
-  produtos : [
-  {
-    nome : "Pão",
-    precounit : 100,
-    quantidade : 3
-  }, {
-    nome : "Queijo",
-    precounit : 500,
+    precounit : 5000,
     quantidade : 5
   }, {
     nome : "Leite",
-    precounit : 900,
+    precounit : 9000,
     quantidade : 3
   }
   
-  ]
-};
+  ]; 
+
+/*const cartao = {
+  nome = "Flavio",
+  idade : 41,
+  produtos : [
+   {
+    nome : "Pão",
+    precounit : 100,
+    quantidade : 3
+   }, {
+    nome : "Queijo",
+    precounit : 500,
+    quantidade : 5
+   }, {
+    nome : "Leite",
+    precounit : 900,
+    quantidade : 3
+   }
+    ]
+};*/
 
 let total = 0;
 
-/*for (let i = 0; i<produtos.length; i++){
+for (let i = 0; i<produtos.length; i++){
   total += produtos[i].precounit * produtos[i].quantidade;
 }
-console.log (`Cliente : ${cartao.nome}
+/*console.log (`Cliente : ${cartao.nome}
 total a pagar : ${total}`); */
-console.log(cartao);
+console.log(produtos);
+console.log(total/1000);
