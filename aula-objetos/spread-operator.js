38a81acdf3216e6829cdda343ef5ebcdd2ee18d9
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-22 17:51:22 -0300

    update spread-operator.js

--- aula-objetos/spread-operator.js
+++ aula-objetos/spread-operator.js
@@ -1,1 +1,23 @@
+// Spread ...
+// espalhas os objetos
+// exemplo
 
+const pessoa ={
+  nome : "Flávio",
+  idade : 41,
+  cidade : "Rio de janeiro"
+}
+
+const endereco ={
+  rua: "Rodrigues Campelo",
+  numero : 47,
+  complemento : "quadra 6"
+};
+
+const propriedadeFundida ={
+  ... pessoa,
+  ... endereco,
+  bairro : "Campo grande"
+};
+
+console.log(propriedadeFundida);
