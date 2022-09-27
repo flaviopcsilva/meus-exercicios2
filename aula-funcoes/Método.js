8c01d82ab748a64cf38a2962d75022ccfa7275d1
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-09-27 07:01:32 -0300

    update Método.js

--- aula-funcoes/Método.js
+++ aula-funcoes/Método.js
@@ -1,1 +1,20 @@
+const pessoa{
+  nome : "Flávio",
+  idade : 41,
+  profissao : "DEV",
+  apresentar : function(){
+    const faixaEtaria = this.determinarFaixaEtaria(this.idade);
+    console.log(`Sou ${this.nome}, sou um(a) ${faixaEtaria} de ${this.idade} anos e sou ${this.profissao}`);
+  },
+  determinarFaixaEtaria : function(){
+    if(this.idade<=21){
+      return "Jovem";
+    }else if(this.idade<66){
+      return "Adulto";
+    }else{
+      return "Idoso";
+    }
+  }
+}
 
+pessoa.apresentar();
