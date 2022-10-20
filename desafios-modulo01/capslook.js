06a048903a77103abdc73fe98be0d7ae2d15b4e9
flaviopcsilva <112036441+flaviopcsilva@users.noreply.github.com>
2022-10-20 12:20:46 -0300

    update capslook.js

--- desafios-modulo01/capslook.js
+++ desafios-modulo01/capslook.js
@@ -1,52 +1,52 @@
 function solucao(input) {
 
-  // let letra = input.slice(1, 2);
-  // if (letra === letra.toUpperCase()) {
-  //     console.log("letra maiuscula");
-  //     letra = input.slice(0, 1).toUpperCase() + input.slice(1).toLowerCase();
-  //     console.log(letra);
+    // let letra = input.slice(1, 2);
+    // if (letra === letra.toUpperCase()) {
+    //     console.log("letra maiuscula");
+    //     letra = input.slice(0, 1).toUpperCase() + input.slice(1).toLowerCase();
+    //     console.log(letra);
 
-  // } else {
-  //     console.log("letra minuscula");
-  //     letra = input.slice(0, 1) + input.slice(1).toLowerCase();
-  //     console.log(letra);
-  // }
+    // } else {
+    //     console.log("letra minuscula");
+    //     letra = input.slice(0, 1) + input.slice(1).toLowerCase();
+    //     console.log(letra);
+    // }
 
-  // const arrayLetra = input.split("");
-  // let letra = "";
-  // if (input === input.toLowerCase()) {
-  //     console.log(input);
-  // } else {
-  //     letra = arrayLetra[0].toUpperCase();
-  //     for (let i = 1; i < arrayLetra.length; i++) {
-  //         letra += arrayLetra[i].toLowerCase();
-  //     }
-  //     console.log(letra);
-  // }
+    // const arrayLetra = input.split("");
+    // let letra = "";
+    // if (input === input.toLowerCase()) {
+    //     console.log(input);
+    // } else {
+    //     letra = arrayLetra[0].toUpperCase();
+    //     for (let i = 1; i < arrayLetra.length; i++) {
+    //         letra += arrayLetra[i].toLowerCase();
+    //     }
+    //     console.log(letra);
+    // }
 
 
-  let letraFormatada = "";
-  const primeiraLetra = input.slice(0, 1);
-  const restoDaPalavra = input.slice(1);
-  //console.log(restoDaPalavra);
+    let letraFormatada = "";
+    const primeiraLetra = input[0];
+    const restoDaPalavra = input.slice(1);
+    //console.log(restoDaPalavra);
 
-  if (primeiraLetra === primeiraLetra.toLowerCase() && restoDaPalavra === restoDaPalavra.toUpperCase()) {
-    letraFormatada = primeiraLetra.toUpperCase() + restoDaPalavra.toLowerCase();
-    console.log(letraFormatada);
-  } else if (input === input.toUpperCase()) {
-    console.log(input.toLowerCase);
-  }
-  else {
-    console.log(input);
-  }
-  /* if(primeiraLetra===primeiraLetra.toLowerCase() && restoDaPalavra===restoDaPalavra.toUpperCase()){
-  letraFormatada=primeiraLetra.toUpperCase()+restoDaPalavra.toLowerCase();
-  console.log(letraFormatada);
-  
-  } else if(input===input.toUpperCase()){
-    console.log(input.toLowerCase());
-  }else{
-    console.log(input);*/
+   if (primeiraLetra === primeiraLetra.toLowerCase() && restoDaPalavra === restoDaPalavra.toUpperCase()) {
+       letraFormatada = primeiraLetra.toUpperCase() + restoDaPalavra.toLowerCase();
+       console.log(letraFormatada);
+ } else if(input===input.toUpperCase()){
+   console.log(input.toLowerCase());
+ }
+ else{
+      console.log(input);
+   }
+/* if(primeiraLetra===primeiraLetra.toLowerCase() && restoDaPalavra===restoDaPalavra.toUpperCase()){
+letraFormatada=primeiraLetra.toUpperCase()+restoDaPalavra.toLowerCase();
+console.log(letraFormatada);
+
+} else if(input===input.toUpperCase()){
+  console.log(input.toLowerCase());
+}else{
+  console.log(input);*/
 }
 
 solucao("cAPS");
