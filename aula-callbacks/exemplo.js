a6fa0123dade07ff9aa1ee54b92f15f12e16dd7d
flaviopcsilva <flaviopc2@gmail.com>
2022-10-28 20:02:14 -0300

    Testando Prompt

--- aula-callbacks/exemplo.js
+++ aula-callbacks/exemplo.js
@@ -1,5 +1,22 @@
-const nome = require('prompt-sync')({ sigint: true });
-let x;
-x = nome("DIgite o vaolr de x:");
+const nome = require('prompt-sync')({ sigint: false });
 
-console.log(parseInt(x));
+const nome2 = require('prompt-sync')({ sigint: false });
+const array = [];
+let teste;
+while (teste != "Sair") {
+    teste = nome("Digite um número ou Sair: ");
+    if (teste === "Sair") {
+        break;
+    }
+    if (teste === "") {
+        console.log("Não pode ser valor em branco.");
+    } else {
+        array.push(teste);
+    }
+}
+console.log(array.join(", "));
+
+
+
+
+
