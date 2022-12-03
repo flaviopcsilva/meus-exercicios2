217b86a9ebed5308fb79b2f015070e9336fcb121
flaviopcsilva <flaviopc2@gmail.com>
2022-12-03 19:23:24 -0300

    Aula de API´s Rest

---
+++ aula-apis-Rest/serv-ApiRest/src/bancodedados.js
@@ -0,0 +1,19 @@
+const bancoDeDados = {
+    identificadorInstrutor: 3,
+    instrutores: [
+        {
+            id: 1,
+            nome: 'Guido',
+            email: 'guiodo@email.com',
+            status: true
+        },
+        {
+            id: 2,
+            nome: 'Dani',
+            email: 'Dani@email.com',
+            status: true
+        }
+    ]
+}
+
+module.exports = bancoDeDados;
