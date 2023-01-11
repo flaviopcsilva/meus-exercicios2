1d0e29563615fcbe47461411f9e89175679a0479
flaviopcsilva <flaviopc2@gmail.com>
2023-01-11 10:36:59 -0300

    Refatorando desafio modulo 02.

--- desafio-modulo02/src/controladores/operacoes-bancarias.js
+++ desafio-modulo02/src/controladores/operacoes-bancarias.js
@@ -1,4 +1,5 @@
 const fs = require('fs/promises');
+const { format } = require('date-fns');
 
 
 const listarContas = async (req, res) => {
@@ -56,9 +57,59 @@ const cadastrarConta = async (req, res) => {
     } catch (error) {
         return res.status(500).json({ Mensagem: error.message });
     }
+};
+
+//   Depósito
+const deposito = async (req, res) => {
+    const { numero_conta, valor } = req.body;
+    try {
+        //importando dados JSON
+        const banco = await fs.readFile('./src/dados/bancodedados.json');
+        let contas = JSON.parse(banco);
+
+        //Verificar se os valores foram declarados no body
+        if (!numero_conta) {
+            return res.status(400).json({ Mensagem: 'O número da conta é obrigatório.' });
+        };
+        //verificando se foi declarado ou se é menor ou igual a zero
+        if (!valor || valor < 0) {
+            return res.status(400).json({ Mensagem: 'O valor é obrigatório ou não pode ser menor ou igual a zero.' });
+        };
+
+        const contaEncontrada = contas.contas.find((numConta) => {
+            return numConta.numeroDaConta === Number(numero_conta);
+        });
+
+        if (!contaEncontrada) {
+            return res.status(404).json({ Mensagem: 'Conta inválida.' });
+        };
+
+        //cadastrando deposito com data
+        const novaData = new Date();
+        const dataFormatada = format(novaData, 'dd-MM-yy HH:mm:ss');
+
+        const novoDeposito = {
+            data: dataFormatada,
+            numero_conta,
+            valor
+        };
+
+        contas.depositos.push(novoDeposito);
+        contaEncontrada.saldo += valor;
+        //gravando no arquivo .json
+        await fs.writeFile('./src/dados/bancodedados.json', JSON.stringify(contas));
+
+
+        return res.status(201).send();
+
+
+    } catch (error) {
+        return res.status(500).json({ Mensagem: error.message });
+    }
 }
 
 module.exports = {
     listarContas,
-    cadastrarConta
+    cadastrarConta,
+    deposito
 };
--- desafio-modulo02/src/rotas.js
+++ desafio-modulo02/src/rotas.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { listarContas, cadastrarConta } = require('./controladores/operacoes-bancarias');
+const { listarContas, cadastrarConta, deposito } = require('./controladores/operacoes-bancarias');
 
 const rotas = express.Router();
 
 rotas.get('/contas', listarContas);
 rotas.post('/contas', cadastrarConta);
+rotas.post('/transacoes/depositar', deposito);
 
 module.exports = rotas;
